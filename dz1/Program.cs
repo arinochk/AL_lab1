4a38c936b9cb0c9244678b0be51aeb7e6d155e6c
arinacherednik <arina.cherednik@yandex.ru>
2022-09-19 20:25:42 +0300

    Second commit

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -11,6 +11,9 @@ namespace exercise2
             Console.WriteLine($"Обьект: Int16;  принимает от {int.MinValue}  до {int.MaxValue}  ");
             Console.WriteLine($"Обьект: UInt16;  принимает от {uint.MinValue}  до {uint.MaxValue}  ");
             Console.WriteLine($"Обьект: UInt64;  принимает от {long.MinValue}  до {long.MaxValue}  ");
+            Console.WriteLine($"Обьект: Decimal;  принимает от {decimal.MinValue}  до {decimal.MaxValue}  ");
+            Console.WriteLine($"Обьект: Float;  принимает от {float.MinValue}  до {float.MaxValue}  ");
+            Console.WriteLine($"Обьект: Double;  принимает от {double.MinValue}  до {double.MaxValue}  ");
         }
 
     }
